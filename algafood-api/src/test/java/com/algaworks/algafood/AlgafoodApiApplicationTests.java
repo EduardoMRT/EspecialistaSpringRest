75426236742a6cff2b554c378490b36cbe199433
EduardoMRT <eduksoficial@gmail.com>
2024-02-06 15:06:14 -0300

    alt

---
+++ algafood-api/src/test/java/com/algaworks/algafood/AlgafoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algaworks.algafood;
+
+import org.junit.Ignore;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgafoodApiApplicationTests {
+	@Ignore
+	@org.junit.Test
+	void contextLoads() {
+	}
+
+}
